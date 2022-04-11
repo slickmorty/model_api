8fe3590902e1ee47f7a50e05e90cb3f088d61c09
Ahmad Abasi <a_abasi97@yahoo.com>
2022-04-11 06:40:52 +0430

    Added Settings

--- data/get_data.py
+++ data/get_data.py
@@ -1,7 +1,20 @@
+from datetime import datetime
+import MetaTrader5 as mt5
 
 
-def get_initial_data():
-    return
+def get_initial_data(model_date: str, symbol: str):
+
+    model_date = datetime.strptime(model_date, "%y-%m-%d %H:%M:%S")
+    if not mt5.initialize():
+        print("initialize() failed")
+        mt5.shutdown()
+
+    data1 = mt5.copy_rates_range(
+        symbol, mt5.TIMEFRAME_M5, model_date, datetime.now())
+
+    data2 = mt5.copy_rates_from(symbol, mt5.TIMEFRAME_M5, model_date, 256)
+
+    return data1, data2
 
 
 def get_live_data():
@@ -10,3 +23,10 @@ def get_live_data():
 
 def load_data_from_csv():
     return
+
+
+if __name__ == "__main__":
+
+    model_name = "22-03-10 19:31:00"
+    a, b = get_initial_data(model_name, "SP500m")
+    print(len(b))
