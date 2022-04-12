812a6ebfbb323c7f2e61161b3fc3a5455363ff41
Ahmad Abasi <a_abasi97@yahoo.com>
2022-04-12 05:38:44 +0430

    Made column names in settings

--- data/settings.py
+++ data/settings.py
@@ -6,3 +6,5 @@ with open("./data/settings.json", "r") as f:
 
 symbol = data.get("data").get("symbol")
 how_many_candles_before = data.get("data").get("how_many_candles_before")
+time_difference_in_seconds = data.get("data").get("time_difference_in_seconds")
+column_names = data.get("data").get("column_names")
