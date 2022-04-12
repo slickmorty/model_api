fa08ee4aee484a28cd85aed634007bdc510fb927
Ahmad Abasi <a_abasi97@yahoo.com>
2022-04-12 06:36:27 +0430

    Added candles with nan number

--- data/settings.py
+++ data/settings.py
@@ -6,5 +6,6 @@ with open("./data/settings.json", "r") as f:
 
 symbol = data.get("data").get("symbol")
 how_many_candles_before = data.get("data").get("how_many_candles_before")
+candles_with_nan = data.get("date").get("candles_with_nan")
 time_difference_in_seconds = data.get("data").get("time_difference_in_seconds")
 column_names = data.get("data").get("column_names")
