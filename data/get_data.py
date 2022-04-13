0fcbd7086569701f1fe7b08fe99eaa9f64b0b39a
Ahmad Abasi <a_abasi97@yahoo.com>
2022-04-13 10:28:11 +0430

    Added 2 more fucntions and comments

--- data/get_data.py
+++ data/get_data.py
@@ -1,14 +1,23 @@
 from datetime import datetime
-from typing import List
+from symtable import Symbol
 import numpy as np
 import MetaTrader5 as mt5
 from pytz import timezone
 import data.settings as data_settings
 import model.settings as model_settings
+import pandas as pd
 
 
-def get_initial_data(model_date: str = model_settings.model_name, symbol: str = data_settings.symbol) -> tuple[List, List]:
+def get_initial_data(model_date: str = model_settings.model_name, symbol: str = data_settings.symbol) -> tuple[list, list]:
+    """Getting initial data
 
+    Args:
+        model_date (str, optional): The date model was previously trained on. Defaults to model_settings.model_name.
+        symbol (str, optional): symbol e.g. SP500m. Defaults to data_settings.symbol.
+
+    Returns:
+        tuple[list, list]: A tuple containing list of data untill now and some data before model's date
+    """
     # Make model date's time compatible with metatradertimesone
     model_date = datetime.strptime(model_date, "%y-%m-%d %H:%M:%S")
     model_date = convert_to_metatrader_timezone(model_date)
@@ -28,8 +37,33 @@ def get_initial_data(model_date: str = model_settings.model_name, symbol: str = 
     return data_until_now.tolist(), data_before_model_date.tolist()
 
 
-def get_live_data():
-    return
+def get_live_data(symbol: str = data_settings.symbol) -> list[tuple]:
+    """Getting Live data
+
+    Args:
+        symbol (str, optional): symbol e.g. SP500m. Defaults to data_settings.symbol.
+
+    Returns:
+        list[tuple]: A list containing a tuple containing data.
+    """
+    live_data = mt5.copy_rates_from_pos(symbol, mt5.TIMEFRAME_M5, 0, 1)
+
+    return live_data.tolist()
+
+
+def get_prev_candle(symbol: str = data_settings.symbol) -> tuple:
+    """Getting previous candle
+
+    Args:
+        symbol (str, optional): symbol e.g. SP500m. Defaults to data_settings.symbol.
+
+    Returns:
+        list[tuple]: A tuple containing data.
+    """
+    prev_candle = mt5.copy_rates_from(
+        symbol, mt5.TIMEFRAME_M5, convert_to_metatrader_timezone(datetime.now()), 2)
+
+    return prev_candle.tolist()[:][0]
 
 
 def load_data_from_csv():
