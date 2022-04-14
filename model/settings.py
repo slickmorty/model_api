96f1812b11d85320678d5ea4b94f1fc7ff574b6c
Ahmad Abasi <a_abasi97@yahoo.com>
2022-04-14 11:51:32 +0430

    Added base model path but not sure

--- model/settings.py
+++ model/settings.py
@@ -4,4 +4,9 @@ import data.settings as data_settings
 with open("./model/settings.json", "r") as f:
     data: dict = json.load(f)
 
-model_name = data.get("model").get("name")
+model_date = data.get("model").get("date")
+model_path = data.get("model").get("model_path")
+base_model_path = data.get("model").get("base_model_path")
+epoches = data.get("model").get("epoches")
+loss = data.get("model").get("loss")
+optimizer = data.get("model").get("optimizer")
