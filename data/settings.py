16ba3b78d6408c94f3309b29e4f28706c52f8542
Ahmad Abasi <a_abasi97@yahoo.com>
2022-04-15 13:50:09 +0430

    Added save function and cleanded

--- data/settings.py
+++ data/settings.py
@@ -4,14 +4,22 @@ import json
 with open("./data/settings.json", "r") as f:
     data: dict = json.load(f)
 
-symbol = data.get("symbol")
-how_many_candles_before = data.get("how_many_candles_before")
-candles_with_nan = data.get("candles_with_nan")
-time_difference_in_seconds = data.get("data").get("time_difference_in_seconds")
-column_names = data.get("column_names")
-raw_data_csv_path = data.get("raw_data_csv_path")
-indicator_data_csv_path = data.get("data").get("indicator_data_csv_path")
-indicators = data.get("indicators")
-candle_params = data.get("candle_params")
-window_size = data.get("window_size")
-future_window_size = data.get("future_window_size")
+symbol: str = data.get("symbol")
+how_many_candles_before: int = data.get("how_many_candles_before")
+candles_with_nan: int = data.get("candles_with_nan")
+time_difference_in_seconds: int = data.get(
+    "data").get("time_difference_in_seconds")
+column_names: list[str] = data.get("column_names")
+raw_data_csv_path: str = data.get("raw_data_csv_path")
+indicator_data_csv_path: str = data.get("data").get("indicator_data_csv_path")
+indicators: list[str] = data.get("indicators")
+candle_params: list[str] = data.get("candle_params")
+window_size: int = data.get("window_size")
+future_window_size: int = data.get("future_window_size")
+
+
+def save(param, param_name: str, data: dict = data):
+    data[f"{param_name}"] = param
+
+    with open("./data/settings.json", "w") as f:
+        json.dump(data, f)
