2cb4d4c82e994b4637a83319ffda2145244d17c8
Ahmad Abasi <a_abasi97@yahoo.com>
2022-04-30 03:40:50 +0430

    Removed old files and mixed them up in api_requests

---
+++ api/api_requests.py
@@ -0,0 +1,65 @@
+import json
+import pandas as pd
+import requests
+from api.settings import api_settings
+
+
+def delete_all() -> requests.Response:
+
+    url = api_settings.delete_many
+    payload = json.dumps({
+        "collection": api_settings.collection,
+        "database": api_settings.database,
+        "dataSource": api_settings.data_source,
+        "filter": {}
+    })
+    headers = {
+        "Content-Type": api_settings.content_type,
+        "Access-Control-Request-Headers": api_settings.access_control_request_headers,
+        "api-key": api_settings.api_key
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response
+
+
+def insert_all(df: pd.DataFrame) -> requests.Response:
+
+    url = api_settings.insert_many
+
+    payload = json.dumps({
+        "collection": api_settings.collection,
+        "database": api_settings.database,
+        "dataSource": api_settings.data_source,
+        "documents": df.to_dict("records")
+    })
+    headers = {
+        "Content-Type": api_settings.content_type,
+        "Access-Control-Request-Headers": api_settings.access_control_request_headers,
+        "api-key": api_settings.api_key
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response
+
+
+def insert_one(df: pd.DataFrame) -> requests.Response:
+
+    if(len(df) > 1):
+        return
+    url = api_settings.insert_one
+
+    payload = json.dumps({
+        "collection": api_settings.collection,
+        "database": api_settings.database,
+        "dataSource": api_settings.data_source,
+        "documents": df.to_dict("records")
+    })
+    headers = {
+        "Content-Type": api_settings.content_type,
+        "Access-Control-Request-Headers": api_settings.access_control_request_headers,
+        "api-key": api_settings.api_key
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response
