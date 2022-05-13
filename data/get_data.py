03e48ef8b75bf829e944c131dbcbfcf3075a86ca
Ahmad Abasi <a_abasi97@yahoo.com>
2022-05-13 21:00:15 +0430

    Adapted it for when network goes down

--- data/get_data.py
+++ data/get_data.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 import MetaTrader5 as mt5
+import pandas as pd
+import time
+
 from data.settings import data_settings
 from model.settings import model_settings
 from data import indicators
-import pandas as pd
+from logs import logs
+from main import mylogs
 
 
 def get_initial_data(model_date: datetime = model_settings.model_date, symbol: str = data_settings.symbol) -> tuple[list, list]:
@@ -25,14 +29,20 @@ def get_initial_data(model_date: datetime = model_settings.model_date, symbol: s
         mt5.shutdown()
 
     now = convert_to_metatrader_timezone(datetime.now())
+    while True:
+        try:
+            data_until_now = mt5.copy_rates_range(
+                symbol, mt5.TIMEFRAME_M5, model_date, now).tolist()
 
-    data_until_now = mt5.copy_rates_range(
-        symbol, mt5.TIMEFRAME_M5, model_date, now)
+            data_before_model_date = mt5.copy_rates_from(
+                symbol, mt5.TIMEFRAME_M5, model_date, data_settings.how_many_candles_before).tolist()
+        except:
+            mylogs.exception(logs.ERROR_RETRIEVING_DATA_FROM_META)
+            time.sleep(1)
+        else:
+            break
 
-    data_before_model_date = mt5.copy_rates_from(
-        symbol, mt5.TIMEFRAME_M5, model_date, data_settings.how_many_candles_before)
-
-    return data_until_now.tolist(), data_before_model_date.tolist()
+    return data_until_now, data_before_model_date
 
 
 def get_live_data(symbol: str = data_settings.symbol) -> list[tuple]:
@@ -47,9 +57,19 @@ def get_live_data(symbol: str = data_settings.symbol) -> list[tuple]:
     if not mt5.initialize():
         print("initialize() failed")
         mt5.shutdown()
-    live_data = mt5.copy_rates_from_pos(symbol, mt5.TIMEFRAME_M5, 0, 1)
 
-    return live_data.tolist()
+    # For when internet goes down
+    while True:
+        try:
+            live_data = mt5.copy_rates_from_pos(
+                symbol, mt5.TIMEFRAME_M5, 0, 1).tolist()
+        except Exception:
+            mylogs.exception(logs.ERROR_RETRIEVING_DATA_FROM_META)
+            time.sleep(1)
+        else:
+            break
+
+    return live_data
 
 
 def get_prev_candle(symbol: str = data_settings.symbol) -> tuple:
@@ -64,10 +84,18 @@ def get_prev_candle(symbol: str = data_settings.symbol) -> tuple:
     if not mt5.initialize():
         print("initialize() failed")
         mt5.shutdown()
-    prev_candle = mt5.copy_rates_from(
-        symbol, mt5.TIMEFRAME_M5, convert_to_metatrader_timezone(datetime.now()), 2)
 
-    return prev_candle.tolist()[:][0]
+    # For when internet goes down
+    while True:
+        try:
+            prev_candle = mt5.copy_rates_from(
+                symbol, mt5.TIMEFRAME_M5, convert_to_metatrader_timezone(datetime.now()), 2).tolist()[:][0]
+        except Exception:
+            mylogs.exception(logs.ERROR_RETRIEVING_DATA_FROM_META)
+            time.sleep(1)
+        else:
+            break
+    return prev_candle
 
 
 def convert_to_metatrader_timezone(date: datetime) -> datetime:
